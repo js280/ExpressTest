1620a244bc38247edb8558db87807225d41fc619
JS280 <js_280@yahoo.com>
2017-07-09 18:12:50 -0500

    Added new files for bootstrap and mongodb integration

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/mongo_test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/blog', function(req, res, next) {
-	res.render('blog', {title: 'Blog Page', main_element: "Test Item 1", secondary_element: "Test Item 2"});
+router.get('/customers', function(req, res, next) {
+	MongoClient.connect(url, function(err, db){
+		if(err) {
+			console.log(err);
+		} else {
+			var collection = db.collection('customers');
+			var myData = collection.find().limit(100).toArray(function(err, docs){
+				res.render('customers', {title: "Customer List", data: docs});
+				console.log(__dirname);
+			});
+			
+		}
+	});
+});
+
+router.post('/customers', function(req, res, next) {
+	MongoClient.connect(url, function(err, db) {
+		if(err) {
+			console.log(err);
+		} else {
+			var collection = db.collection('customers');
+			collection.insertOne({first_name : req.body.first_name, last_name : req.body.last_name});
+			res.redirect('/customers');
+			db.close();
+		}
+	});
 });
 
 module.exports = router;
