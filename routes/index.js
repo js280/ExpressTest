02ddccef237061fdf80de35bce9a3866c873ab1c
JS280 <js_280@yahoo.com>
2017-07-11 18:38:40 -0500

    Made some changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/mongo_test';
+var URL = require('url');
+var mongo_url = 'mongodb://localhost:27017/mongo_test';
+var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,13 +11,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/customers', function(req, res, next) {
-	MongoClient.connect(url, function(err, db){
+	MongoClient.connect(mongo_url, function(err, db){
 		if(err) {
 			console.log(err);
 		} else {
 			var collection = db.collection('customers');
 			var myData = collection.find().limit(100).toArray(function(err, docs){
-				res.render('customers', {title: "Customer List", data: docs});
+				res.render('customers', {title: "Customer List", err_msg: "", data: docs});
 				console.log(__dirname);
 			});
 			
@@ -23,13 +25,27 @@ router.get('/customers', function(req, res, next) {
 	});
 });
 
-router.post('/customers', function(req, res, next) {
-	MongoClient.connect(url, function(err, db) {
+router.post('/customers/add', function(req, res, next) {
+	MongoClient.connect(mongo_url, function(err, db) {
 		if(err) {
 			console.log(err);
 		} else {
 			var collection = db.collection('customers');
 			collection.insertOne({first_name : req.body.first_name, last_name : req.body.last_name});
+			res.redirect('/customers');
+			db.close();
+		}
+	});
+});
+
+router.get('/customers/remove', function(req, res, next) {
+	MongoClient.connect(mongo_url, function(err, db) {
+		if(err) {
+			console.log(err);
+		} else {
+			var collection = db.collection('customers');
+			collection.removeOne({first_name : req.body.first_name, last_name : req.body.last_name});
+			console.log(req.body);
 			res.redirect('/customers');
 			db.close();
 		}
